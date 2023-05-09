50c1a7db3e224b486009c27855678b59a95a5439
J0hnux <jc24.doliente@gmail.com>
2023-05-09 23:38:06 +0800

    styled to flex

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,9 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-green-900 min-h-screen">
+    <div className="bg-green-900 min-h-screen flex">
       <Nav />
-      <div>logged in as {session.user.email}</div>
+      <div className="bg-white flex-grow p-4 mt-2 mr-2 mb-2 rounded-lg">logged in as {session.user.email}</div>
     </div>
     
   )
