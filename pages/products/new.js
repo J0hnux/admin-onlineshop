7f4dd05cfc3f5db72b335e77709db2a1793b3be3
J0hnux <jc24.doliente@gmail.com>
2023-05-10 00:09:40 +0800

    created the file and added input

---
+++ pages/products/new.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+
+export default function New() {
+    return (
+        <Layout>
+            <input type="text" placeholder="product name" />
+        </Layout>
+    )
+}
