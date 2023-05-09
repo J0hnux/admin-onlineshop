9ad1bc6d72e3a4847594b0d71c72c86348cecbad
J0hnux <jc24.doliente@gmail.com>
2023-05-10 00:02:44 +0800

    designed logged in message with image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <Layout>
-      test
+      <div className="text-green-900 flex justify-between">
+        <div className="font-medium">
+          Hello, <b>{session?.user?.name}</b>
+        </div>
+
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
+      </div>
     </Layout>
   )
 }
