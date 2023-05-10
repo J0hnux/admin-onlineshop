c5dce1ec21edb901f14718dc5cabaf36cc80098a
J0hnux <jc24.doliente@gmail.com>
2023-05-10 14:01:26 +0800

    implemented react state

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,45 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function New() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+
+    async function createProduct() {
+        const data = {title,description,price};
+        await axios.post('/api/products', data);
+    }
+    
     return (
         <Layout>
-            <h1>New Product</h1>
-            <label>Product Name</label>
-            <input type="text" placeholder="product name" />
-            <label>Description</label>
-            <textarea placeholder="description"></textarea>
-            <label>Price (in USD)</label>
-            <input type="number" placeholder="price" />
-            <button className="btn-primary">Save</button>
+            <form onSubmit={createProduct}>
+                <h1>New Product</h1>
+                <label>Product Name</label>
+                <input 
+                    type="text" 
+                    placeholder="product name" 
+                    value={title} 
+                    onChange={ev => setTitle(ev.target.value)}
+                />
+                <label>Description</label>
+                <textarea 
+                    placeholder="description" 
+                    value={description} 
+                    onChange={ev => setDescription(ev.target.value)}>
+                </textarea>
+                <label>Price (in USD)</label>
+                <input 
+                    type="number" 
+                    placeholder="price" 
+                    value={price} 
+                    onChange={ev => setPrice(ev.target.value)}
+                />
+                <button 
+                    type="submit" 
+                    className="btn-primary">Save</button>
+            </form>
         </Layout>
     )
 }
