642a38a0399c3da0297ee465027248d3d5f7e4d8
J0hnux <jc24.doliente@gmail.com>
2023-05-10 20:35:14 +0800

    Redirect user back to products page upon successful of product creation

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,24 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
 import { useState } from "react";
 
 export default function New() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
+    const [goToProducts, setGoToProducts] = useState(false);
 
     async function createProduct(ev) {
         ev.preventDefault();
         const data = {title,description,price};
         await axios.post('/api/products', data);
+        setGoToProducts(true);
+
+    }
+
+    if(goToProducts) {
+        return redirect('/products');
     }
     
     return (
