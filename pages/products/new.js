e5d62200ec9aea195422513c3f15e93e37b754a1
J0hnux <jc24.doliente@gmail.com>
2023-05-10 13:44:55 +0800

    added labels for the inputs, and added few more inputs and buttons

--- pages/products/new.js
+++ pages/products/new.js
@@ -3,8 +3,14 @@ import Layout from "@/components/Layout";
 export default function New() {
     return (
         <Layout>
+            <h1>New Product</h1>
+            <label>Product Name</label>
             <input type="text" placeholder="product name" />
+            <label>Description</label>
             <textarea placeholder="description"></textarea>
+            <label>Price (in USD)</label>
+            <input type="number" placeholder="price" />
+            <button className="btn-primary">Save</button>
         </Layout>
     )
 }
