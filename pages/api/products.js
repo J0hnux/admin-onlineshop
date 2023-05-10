3ec08c295bb4adfcaedc280f243f19fd2ba20b58
J0hnux <jc24.doliente@gmail.com>
2023-05-10 14:44:07 +0800

    implemented mongooseConnect()

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,10 @@
-import { Product } from "@/models/Product";
 import mongoose from "mongoose";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
     const {method} = req;
-    
+    await mongooseConnect();
 
     if(method === 'POST') {
         const {title, description, price} = req.body;
